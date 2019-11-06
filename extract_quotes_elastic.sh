f3146b6219c9c270b8683a5d55d4b192bb77512d
suhel ajjaman <ajjaman.suhel@gmail.com>
2019-11-06 18:12:33 +0530

    Update extract_quotes_elastic.sh

--- extract_quotes_elastic.sh
+++ extract_quotes_elastic.sh
@@ -1,4 +1,4 @@
 !/bin/bash
 for i in {1..255}
-        do curl -s http://10.10.10.115:9200/quotes/_search?size=$i |jq | grep  '"quote":'
+        do curl -s http://10.10.10.115:9200/quotes/_search?size=$i |jq | grep  '"quote":'| cut -d ":" -f 2
         done
